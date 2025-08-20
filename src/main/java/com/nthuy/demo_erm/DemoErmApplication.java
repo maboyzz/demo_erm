317406818ee6b367786c22819479aab8987b74b1
NT HUY <144404737+maboyzz@users.noreply.github.com>
2025-08-20 16:00:22 +0700

    setup project

---
+++ src/main/java/com/nthuy/demo_erm/DemoErmApplication.java
@@ -0,0 +1,13 @@
+package com.nthuy.demo_erm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoErmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoErmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthuy/demo_erm/DemoErmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthuy.demo_erm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoErmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
